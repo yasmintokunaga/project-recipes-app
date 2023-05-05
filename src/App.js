8121454a6aff0853f44c49e11d25eeaa23e2f9ef
jeferson-viana <jefersonv28@gmail.com>
2023-05-05 16:47:34 -0300

    Adicionando a rota /meals  no app.js

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
+import MainMenu from './pages/MainMenu';
 
 function App() {
   return (
     <Switch>
       <Route exact path="/" component={ Login } />
+      <Route exact path="/meals" component={ MainMenu } />
     </Switch>
   );
 }
