483f7c4c2dff486dc302618fdd0d5647594cd9a9
Lílian Vieira <lilianv528@gmail.com>
2023-05-05 18:41:03 -0300

    tela de perfil

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,27 +1,32 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
 
-import whiteHeartIcon from '../images/whiteHeartIcon.svg';
 import profileIcon from '../images/profileIcon.svg';
+import doneIcon from '../images/doneIcon.svg';
+import logoutIcon from '../images/logoutIcon.svg';
+import yellowHearthIcon from '../images/yellowHearthIcon.svg';
 
 export default function Profile() {
   const history = useHistory();
+
+  const getLS = localStorage.getItem('user');
+
+  const handleClick = () => {
+    localStorage.clear();
+    history.push('/');
+  };
+
   return (
     <div>
+      <img src={ profileIcon } alt="profile icon" />
       <h1>PROFILE</h1>
-      <img src={ profileIcon } alt="search icon" />
-      <label htmlFor="email">
-        Email
-        <input
-          type="email"
-          data-testid="profile-email"
-        />
-      </label>
+      <p data-testid="profile-email">{getLS}</p>
       <button
         type="button"
         data-testid="profile-done-btn"
         onClick={ () => history.push('/done-recipes') }
       >
+        <img src={ doneIcon } alt="done icon" />
         Done Recipes
       </button>
 
@@ -30,14 +35,16 @@ export default function Profile() {
         data-testid="profile-favorite-btn"
         onClick={ () => history.push('/favorite-recipes') }
       >
-        <img src={ whiteHeartIcon } alt="search icon" />
+        <img src={ yellowHearthIcon } alt="heart icon" />
         Favorite Recipes
       </button>
 
       <button
         type="button"
         data-testid="profile-logout-btn"
+        onClick={ handleClick }
       >
+        <img src={ logoutIcon } alt="logout icon" />
         Logout
       </button>
     </div>
