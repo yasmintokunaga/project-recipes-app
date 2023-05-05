f7db3a974307d812a4e6906cf6e9c9f19cfcf505
jeferson-viana <jefersonv28@gmail.com>
2023-05-05 16:36:49 -0300

    Requisitos 1 ao 6 concluídos

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { BrowserRouter } from 'react-router-dom';
 import App from '../App';
+import LoginProvider from '../context/LoginProvider';
 
-test('Farewell, front-end', () => {
-  // Este arquivo pode ser modificado ou deletado sem problemas
-  render(<App />);
-  const linkElement = screen.getByText(/TRYBE/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Verificando a funcionalidade da página Login', () => {
+  test('Farewell, front-end', () => {
+    render(
+      <BrowserRouter>
+        <LoginProvider>
+          <App />
+        </LoginProvider>
+      </BrowserRouter>,
+    );
+    const emailElement = screen.getByTestId('email-input');
+    const passwordElement = screen.getByTestId('password-input');
+    const btnElement = screen.getByTestId('login-submit-btn');
+
+    userEvent.type(emailElement, 'teste@teste.com');
+    userEvent.type(passwordElement, '1234567');
+
+    userEvent.click(btnElement);
+  });
 });
