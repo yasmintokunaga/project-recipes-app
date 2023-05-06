b101a5c64984e2e84c0fadea423dd3b1048c0a91
Jeferson Viana <jefersonv28@gmail.com>
2023-05-06 20:47:13 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
 import Login from './pages/Login';
 import Recipes from './pages/Recipes';
