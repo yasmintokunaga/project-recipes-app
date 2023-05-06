952c6c5c546ada1c67f771c6b7568f798b50fbeb
yasmintokunaga <yasmintkng@gmail.com>
2023-05-05 22:53:22 -0300

    feat(REQ21): implements the recipes filter

--- src/components/ButtonsFilterCategories.js
+++ src/components/ButtonsFilterCategories.js
@@ -1,17 +1,25 @@
 import { useContext, useState } from 'react';
 import { RecipesContext } from '../context/RecipesProvider';
-import ListMealsOrDrinks from './RecipesMealsOrDrinks';
+import { fetchRecipesMeals, fetchRecipesDrinks } from '../services/fetchRecipes';
 import { fetchRecepiesByCategoryMeals, fetchRecepiesByCategoryDrinks } from '../services/fetchRecipiesByCategory';
 
 function ButtonsFilterCategories() {
   const { categoriesNames, setListRecipes, numberRecipes } = useContext(RecipesContext);
+  const path = window.location.pathname;
 
   async function handleFilterButton(category) {
-    const listFilterByCategory = window.location.pathname === '/meals'
+    const listFilterByCategory = path === '/meals'
       ? await fetchRecepiesByCategoryMeals(category)
       : await fetchRecepiesByCategoryDrinks(category);
     const configList = listFilterByCategory.filter((_recipe, index) => index < numberRecipes);
     setListRecipes(configList);
+  }
+
+  async function handleRemoveFilter() {
+    const recipes = path === '/drinks'
+    ? await fetchRecipesDrinks() : await fetchRecipesMeals();  
+    const configRecipes = recipes.filter((_recipe, index) => index < numberRecipes);
+    setListRecipes(configRecipes);
   }
 
   return (
@@ -25,6 +33,12 @@ function ButtonsFilterCategories() {
           { category }
         </button>
       ))}
+      <button
+        data-testid="All-category-filter"
+        onClick={ () => handleRemoveFilter() }
+      >
+        Limpar Filtros
+      </button>
     </section>
   );
 }
