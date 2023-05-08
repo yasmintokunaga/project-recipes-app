4fbcf091b285e02b4e465524597349a64c0491d9
jeferson-viana <jefersonv28@gmail.com>
2023-05-08 19:04:26 -0300

    Refatorando o App.test

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -6,51 +6,11 @@ import { Router } from 'react-router-dom';
 import App from '../App';
 import LoginProvider from '../context/LoginProvider';
 import RecipesProvider from '../context/RecipesProvider';
-// import { categoryData, recipesMealsData, categoryMealsData } from '../helper/Data';
 import mockCypress from '../../cypress/mocks/fetch';
 
-// describe('Verificando a funcionalidade da página Login', () => {
-//   beforeEach(() => {
-//     jest.spyOn(global, 'fetch').mockResolvedValue({
-//       json: async () => (categoryData),
-//     }).mockResolvedValueOnce({
-//       json: async () => (recipesMealsData),
-//     }).mockResolvedValueOnce({
-//       json: async () => (categoryMealsData),
-//     });
-//   });
-//   test('Verifica se é possível digitar nos campos email e senha, após isso, clicar no button "Enviar". Espera que a rota mude para "/meals".', async () => {
-//     const history = createMemoryHistory();
-//     render(
-//       <Router history={ history }>
-//         <LoginProvider>
-//           <RecipesProvider>
-//             <App />
-//           </RecipesProvider>
-//         </LoginProvider>
-//       </Router>,
-//     );
-//     const emailElement = screen.getByTestId('email-input');
-//     const passwordElement = screen.getByTestId('password-input');
-//     const btnElement = screen.getByTestId('login-submit-btn');
-
-//     userEvent.type(emailElement, 'teste@teste.com');
-//     userEvent.type(passwordElement, '1234567');
-
-//     userEvent.click(btnElement);
-//     expect(history.location.pathname).toBe('/meals');
-
-//     expect(await screen.findByRole('button', { name: /beef/i })).toBeInTheDocument();
-
-//     const btnsElements = screen.getAllByRole('button');
-//     expect(btnsElements).toHaveLength(8);
-
-//     userEvent.click(btnsElements[0]);
-
-//     const textBeef = await screen.findByText(/beef and mustard pie/i);
-//     expect(textBeef).toBeInTheDocument();
-//   });
-// });
+const EMAIL_INPUT = 'email-input';
+const PASSWORD_INPUT = 'password-input';
+const LOGIN_SUBMIT_BTN = 'login-submit-btn';
 describe('Verificando a funcionalidade da página Login', () => {
   beforeEach(() => {
     fakeFetch = jest.spyOn(global, 'fetch').mockImplementation(mockCypress);
@@ -66,11 +26,11 @@ describe('Verificando a funcionalidade da página Login', () => {
         </LoginProvider>
       </Router>,
     );
-    const emailElement = screen.getByTestId('email-input');
-    const passwordElement = screen.getByTestId('password-input');
-    const btnElement = screen.getByTestId('login-submit-btn');
+    const emailElement = screen.getByTestId(EMAIL_INPUT);
+    const passwordElement = screen.getByTestId(PASSWORD_INPUT);
+    const btnElement = screen.getByTestId(LOGIN_SUBMIT_BTN);
 
-    userEvent.type(emailElement, 'teste@teste.com');
+    userEvent.type(emailElement, 'email@email.com');
     userEvent.type(passwordElement, '1234567');
 
     userEvent.click(btnElement);
@@ -87,9 +47,9 @@ describe('Verificando a funcionalidade da página Login', () => {
         </LoginProvider>
       </Router>,
     );
-    const emailElement = screen.getByTestId('email-input');
-    const passwordElement = screen.getByTestId('password-input');
-    const btnElement = screen.getByTestId('login-submit-btn');
+    const emailElement = screen.getByTestId(EMAIL_INPUT);
+    const passwordElement = screen.getByTestId(PASSWORD_INPUT);
+    const btnElement = screen.getByTestId(LOGIN_SUBMIT_BTN);
 
     userEvent.type(emailElement, 'teste@teste.com');
     userEvent.type(passwordElement, '1234567');
@@ -128,11 +88,11 @@ describe('Verificando a funcionalidade da página Login', () => {
         </LoginProvider>
       </Router>,
     );
-    const emailElement = screen.getByTestId('email-input');
-    const passwordElement = screen.getByTestId('password-input');
-    const btnElement = screen.getByTestId('login-submit-btn');
+    const emailElement = screen.getByTestId(EMAIL_INPUT);
+    const passwordElement = screen.getByTestId(PASSWORD_INPUT);
+    const btnElement = screen.getByTestId(LOGIN_SUBMIT_BTN);
 
-    userEvent.type(emailElement, 'teste@teste.com');
+    userEvent.type(emailElement, 'user@teste.com');
     userEvent.type(passwordElement, '1234567');
 
     userEvent.click(btnElement);
