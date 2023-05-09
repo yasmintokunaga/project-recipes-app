261efb1023bb82ab3c5e7ba9696238dbd40abbff
Rfael-nasm <alysson-242@live.com>
2023-05-08 21:40:40 -0300

    req 8 completo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,22 @@
 import PropTypes from 'prop-types';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 import profileLogo from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
 
 function Header({ title, searchBool }) {
+  const history = useHistory();
+
   return (
     <header>
-      <img
-        src={ profileLogo }
-        alt="clique para ir ao perfil"
+      <button
+        onClick={ () => history.push('/profile') }
         data-testid="profile-top-btn"
-      />
+      >
+        <img
+          src={ profileLogo }
+          alt="clique para ir ao perfil"
+        />
+      </button>
       {searchBool && <img
         src={ searchIcon }
         alt="icone para realizar pesquisa"
