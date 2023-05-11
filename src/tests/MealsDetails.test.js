c2c8177b1f5e09bb0ba2331d70d87be190f96e9a
jeferson-viana <jefersonv28@gmail.com>
2023-05-11 19:12:49 -0300

    Cobertura

--- src/tests/MealsDetails.test.js
+++ src/tests/MealsDetails.test.js
@@ -3,6 +3,7 @@ import { createMemoryHistory } from 'history';
 import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { Router } from 'react-router-dom';
+import copy from 'clipboard-copy';
 import App from '../App';
 import LoginProvider from '../context/LoginProvider';
 import RecipesProvider from '../context/RecipesProvider';
@@ -12,9 +13,11 @@ const EMAIL_INPUT = 'email-input';
 const PASSWORD_INPUT = 'password-input';
 const LOGIN_SUBMIT_BTN = 'login-submit-btn';
 // const PROFILE_TOP_BTN = 'profile-top-btn';
+jest.mock('clipboard-copy');
 describe('Verificando a funcionalidade da página Profile', () => {
   beforeEach(() => {
-    fakeFetch = jest.spyOn(global, 'fetch').mockImplementation(mockCypress);
+    jest.spyOn(global, 'fetch').mockImplementation(mockCypress);
+    copy.mockImplementation(() => {});
   });
   test('Verifica a funcionalidade da rota "/profile".', async () => {
     const history = createMemoryHistory();
@@ -31,23 +34,27 @@ describe('Verificando a funcionalidade da página Profile', () => {
     const emailElement = screen.getByTestId(EMAIL_INPUT);
     const passwordElement = screen.getByTestId(PASSWORD_INPUT);
     const btnElement = screen.getByTestId(LOGIN_SUBMIT_BTN);
-    await waitFor(() => {
-      userEvent.type(emailElement, 'trybe@teste.com');
-      userEvent.type(passwordElement, '1234567');
-      userEvent.click(btnElement);
-    });
-
+    // await waitFor(() => {
+    userEvent.type(emailElement, 'trybe@teste.com');
+    userEvent.type(passwordElement, '1234567');
+    userEvent.click(btnElement);
+    // });
+    console.log('linha 40');
     const linkCorba = await screen.findByText(/corba/i);
-    await waitFor(() => {
-      userEvent.click(linkCorba);
-    });
+    // await waitFor(() => {
+    userEvent.click(linkCorba);
+    // });
+    console.log('linha 45');
     const btnFavorite = await screen.findByRole('button', { name: /favoritar/i });
     const btnShare = await screen.findByTestId('share-btn');
     expect(btnFavorite).toBeInTheDocument();
     expect(btnShare).toBeInTheDocument();
-    // await waitFor(() => {
-    //   userEvent.click(btnShare);
-    // });
-    // expect(screen.getByText(/link copied!/i)).toBeInTheDocument();
+    console.log('linha 50');
+    userEvent.click(btnShare);
+    await waitFor(() => {
+      expect(copy).toBeCalled();
+    });
+    expect(screen.getByText(/link copied!/i)).toBeInTheDocument();
+    console.log('linha 54');
   });
 });
